a9b082af9a1cd08b40de8c95c2ceb4ee4b28f971
khffnsa <16520152@std.stei.itb.ac.id>
2022-03-26 16:27:26 +0700

    update Stats.java

--- src/java/Stats/Stats.java
+++ src/java/Stats/Stats.java
@@ -1,3 +1,9 @@
 public class Stats {
+	private double healthPoint;
+	private double attack;
+	private double defense;
+	private double specialAttack;
+	private double specialDefense;
+	private double speed;
 	
 }
