cd1cbad5ee82dd12b5310734e49898f1d05c0937
akMALE21 <akmalmunggaran.kerja@gmail.com>
2022-03-26 17:12:57 +0700

    menambah konstruktor, atribut, dan getter pada status condition

--- src/java/Status Condition/statusCondition.java
+++ src/java/Status Condition/statusCondition.java
@@ -1,3 +1,30 @@
 public class StatusCondition {
-    
+    //atribut
+    private boolean burn = false;
+    private boolean poison = false;
+    private boolean sleep = false;
+    private boolean paralyze = false;
+
+    //konstruktor
+    public StatusCondition(){
+        this.burn = false;
+        this.poison = false;
+        this.sleep = false;
+        this.paralyze = false;
+    }
+
+    //getter
+    public boolean getBurn(){
+        return this.burn;
+    }
+    public boolean getPoison(){
+        return this.poison;
+    }
+    public boolean getSleep(){
+        return this.sleep;
+    }
+    public boolean getParalyze(){
+        return this.paralyze;
+    }
+
 }
