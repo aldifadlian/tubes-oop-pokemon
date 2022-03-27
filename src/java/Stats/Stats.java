023b63559b4852316df8948e800a41a88a0b4b12
khffnsa <16520152@std.stei.itb.ac.id>
2022-03-27 13:42:43 +0700

    update getter & setter

--- src/java/Stats/Stats.java
+++ src/java/Stats/Stats.java
@@ -6,4 +6,61 @@ public class Stats {
 	private double specialDefense;
 	private double speed;
 	
+	// Construktor
+	public Stats(double hP, double att, double def, double spAtt, double spDeff, double spd) {
+		healthPoint = hP;
+		attack = att;
+		defense = def;
+		specialAttack = spAtt;
+		specialDefense = spDeff;
+		speed = spd;
+	}
+
+	// getter & setter healthPoint
+	public void setHealthPoint(double healthPoint) {
+		this.healthPoint = healthPoint;
+	}
+	public double getHealthPoint() {
+		return healthPoint;
+	}
+
+	// getter & setter attack
+	public void setAttack(double attack) {
+		this.attack = attack;
+	}
+	public double getAttack() {
+		return attack;
+	}
+
+	// getter & setter defense
+	public void setDefense(double defense) {
+		this.defense = defense;
+	}
+	public double getDefense() {
+		return defense;
+	}
+
+	// getter & setter specialAttack
+	public void setSpecialAttack(double specialAttack) {
+		this.specialAttack = specialAttack;
+	}
+	public double getSpecialAttack() {
+		return specialAttack;
+	}
+
+	// getter & setter specialDesefense
+	public void setSpecialDefense(double specialDefense) {
+		this.specialDefense = specialDefense;
+	}
+	public double getSpecialDefense() {
+		return specialDefense;
+	}
+
+	// getter & setter speed
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public double getSpeed() {
+		return speed;
+	}
 }
