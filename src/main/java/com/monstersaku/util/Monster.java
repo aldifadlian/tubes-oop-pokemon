fd840c5b785841c8e14de54d34f03c04654d8931
meandmyabsurdlife <72054686+meandmyabsurdlife@users.noreply.github.com>
2022-04-07 21:05:13 +0700

    add statuscondition attribute + add setter

--- src/main/java/com/monstersaku/util/Monster.java
+++ src/main/java/com/monstersaku/util/Monster.java
@@ -4,20 +4,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Monster {
+    private int id;
     private String nama;
     private List<ElementType> elemenTypes = new ArrayList<ElementType>();
     private Stats baseStats;
-    private List<Move> moves = new ArrayList<Move>();
+    private List<Move> IDmoves = new ArrayList<Move>();
+    private StatusCondition statusCondition;
 
-    /*Kontruktor*/
-    public Monster (String nama, List<ElementType> elemenTypes, Stats baseStats, List<Move> moves) {
+    //Kontruktor
+    public Monster (int id, String nama, List<ElementType> elemenTypes, Stats baseStats, List<Move> IDmoves) {
+        this.id = id;
         this.nama = nama;
         this.elemenTypes = elemenTypes;
         this.baseStats = baseStats;
-        this.moves = moves;
+        this.IDmoves = IDmoves;
+        this.statusCondition = null;
     }
 
-    /*Getter*/
+    //Getter
+    public int getId(){
+        return this.id;
+    }
     public String getName(){
         return this.nama;
     }
@@ -25,10 +32,32 @@ public class Monster {
         return this.elemenTypes;
     }
     public Stats getStats(){ 
-        return (this.baseStats);
+        return this.baseStats;
     }
     public List<Move> getMoves(){
-        return this.moves;
+        return this.IDmoves;
+    }
+    public StatusCondition getStatusCondition(){
+        return this.statusCondition;
     }
 
+    //Setter
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String nama){
+        this.nama = nama;
+    }
+    public void setElemenTypes(List<ElementType> elemenTypes){
+        this.elemenTypes = elemenTypes;
+    }
+    public void setStats(Stats baseStats){ 
+        this.baseStats = baseStats;
+    }
+    public void setMoves(List<Move> IDmoves){
+        this.IDmoves = IDmoves;
+    }
+    public void setStatusCondition(StatusCondition statusCondition){
+        this.statusCondition = statusCondition;
+    }
 }
